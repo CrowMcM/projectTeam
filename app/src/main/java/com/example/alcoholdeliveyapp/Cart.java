a5cf55639c52f59ffa88812a8b822c926bf41f9f
CrowMc2 <eoinmacmahon18@gmail.com>
2019-04-11 13:16:23 +0100

    display order

--- app/src/main/java/com/example/alcoholdeliveyapp/Cart.java
+++ app/src/main/java/com/example/alcoholdeliveyapp/Cart.java
@@ -1,12 +1,9 @@
 package com.example.alcoholdeliveyapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +14,7 @@ public class Cart extends AppCompatActivity implements View.OnClickListener {
     private TextView cvcTxt;
     private TextView dateTxt;
     private TextView etTotal;
+    private TextView displayTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +25,35 @@ public class Cart extends AppCompatActivity implements View.OnClickListener {
 
         payBtn.setOnClickListener(this);
 
+        displayTv =findViewById(R.id.displayTv);
+
+
+        String guin= getIntent().getStringExtra("GUIN_ID");
+        displayTv.setText(guin);
+
+        String heinekin= getIntent().getStringExtra("HEN_ID");
+        displayTv.setText(heinekin);
+
+        String desperados= getIntent().getStringExtra("DOS_ID");
+        displayTv.setText(desperados);
+
+        String foster= getIntent().getStringExtra("FOS_ID");
+        displayTv.setText(foster);
+
+        String captain= getIntent().getStringExtra("CAP_ID");
+        displayTv.setText(captain);
+
+        String smirnoff= getIntent().getStringExtra("SMI_ID");
+        displayTv.setText(smirnoff);
+
+        String basic= getIntent().getStringExtra("BAS_ID");
+        displayTv.setText(basic);
+
+        String wine= getIntent().getStringExtra("WIN_ID");
+        displayTv.setText(wine);
+
+        String whiskey= getIntent().getStringExtra("WHIS_ID");
+        displayTv.setText(whiskey);
     }
 
 
--- app/src/main/java/com/example/alcoholdeliveyapp/Inventory.java
+++ app/src/main/java/com/example/alcoholdeliveyapp/Inventory.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Inventory extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +21,16 @@ public class Inventory extends AppCompatActivity implements View.OnClickListener
         private Button wine;
         private Button whisk;
         private Button cart;
+
+        private TextView guinn;
+        private TextView hen;
+        private TextView desper;
+        private TextView foster;
+        private TextView captain;
+        private TextView smir;
+        private TextView whis;
+        private TextView win;
+        private TextView bas;
 
 
         @Override
@@ -37,16 +49,126 @@ public class Inventory extends AppCompatActivity implements View.OnClickListener
             whisk = findViewById(R.id.whiskeyAdd);
             cart = findViewById(R.id.cartBtn);
 
+            guinn =(TextView) findViewById(R.id.guinTv);
+            hen =(TextView) findViewById(R.id.heinTv);
+            desper =(TextView) findViewById(R.id.desTv);
+            foster =(TextView) findViewById(R.id.fosTv);
+            captain =(TextView) findViewById(R.id.capTv);
+            smir =(TextView) findViewById(R.id.smirnTv);
+            whis =(TextView) findViewById(R.id.whisTv);
+            win =(TextView) findViewById(R.id.wineTv);
+            bas =(TextView) findViewById(R.id.basTv);
+
             cart.setOnClickListener(this);
-            guin.setOnClickListener(this);
-            hein.setOnClickListener(this);
-            des.setOnClickListener(this);
-            fost.setOnClickListener(this);
-            cap.setOnClickListener(this);
-            smirn.setOnClickListener(this);
-            wine.setOnClickListener(this);
-            whisk.setOnClickListener(this);
-            basic.setOnClickListener(this);
+
+            guin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String g = (String) guinn.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("GUIN_ID", g);
+                    startActivity(intent);
+
+                }
+            });
+
+            hein.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String h = (String) hen.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("HEN_ID", h);
+                    startActivity(intent);
+
+                }
+            });
+
+            des.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String d = (String) desper.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("DES_ID", d);
+                    startActivity(intent);
+
+                }
+            });
+
+            fost.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String f = (String) foster.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("FOS_ID", f);
+                    startActivity(intent);
+
+                }
+            });
+
+            cap.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String c = (String) captain.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("CAP_ID", c);
+                    startActivity(intent);
+
+                }
+            });
+
+            smirn.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String s = (String) smir.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("SMI_ID", s);
+                    startActivity(intent);
+
+                }
+            });
+
+            wine.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String w = (String) win.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("WIN_ID", w);
+                    startActivity(intent);
+
+                }
+            });
+
+            whisk.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String k = (String) whis.getText();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("WHIS_ID", k);
+                    startActivity(intent);
+
+                }
+            });
+
+            basic.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String b = (String) bas.getText();
+                    Toast.makeText(getApplicationContext(),b,Toast.LENGTH_LONG).show();
+
+                    Intent intent=new Intent(getBaseContext(),Cart.class);
+                    intent.putExtra("BAS_ID", b);
+                    startActivity(intent);
+
+                }
+            });
 
 
         }
